c008a5b169e07209e67b36e824bb207b427578b4
hk0953307777 <109886653+hk0953307777@users.noreply.github.com>
2022-08-17 02:12:20 +0800

    Update 20220817 Monster腳本

--- My project/Assets/C#/Monster.cs
+++ My project/Assets/C#/Monster.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace pikachu
 {
     public class Monster: MonoBehaviour
     {
-        #region 紅色部分
         [SerializeField, Header("角色剛體")]
         private Rigidbody2D rBody;
+
+        [SerializeField, Header("動畫控制器")]
+        private Animator controller;
 
         [SerializeField, Header("移動速度")]
         [Range(0, 10)]
@@ -16,24 +19,46 @@ namespace pikachu
         [Range(0,500)]
         private int attack = 100;
 
+        [SerializeField]
+        private AudioSource[] attacktone;
+
         [SerializeField, Header("血量")]
         [Range(0, 5000)]
         private int hp = 350;
+
+        [SerializeField, Header("受傷音效")]
+        private AudioClip[] Injuried;
 
         [SerializeField, Header("追蹤範圍")]
         [Range(0, 50)] 
         private int tracking = 30;
 
-        #endregion
+        [SerializeField]
 
+        private TrackingOriginModeFlags target;
+
+        
         [SerializeField, Header("道具掉落")]
         private bool fall;
+
+        [SerializeField, Header("道具掉落音效")]
+        private AudioSource[] sound;
+
+        [SerializeField]
+        private GameObject[] weponprefbs;
         [SerializeField]
         [Range(0, 1)]
         private float probability = 1;
+        
+        
+        
+        
+        
+        
 
+        
 
     }
-  
+
 
 }
